6467c089e12ad6895f7ed9e40a36baaa78c41834
fruit <a.s.kirson@gmail.com>
2021-02-07 22:31:55 +1000

    1. Spring mock [#364902]

--- src/main/java/forum/control/PostControlTest.java
+++ src/main/java/forum/control/PostControlTest.java
@@ -1,26 +1,36 @@
 package forum.control;
 
 import forum.Application;
+import forum.model.Post;
+import forum.service.PostRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 /**
- * @author
+ * @author Andrey
  * @version 1
- * @since
+ * @since 07/02/21
  */
 
 @SpringBootTest(classes = Application.class)
 @AutoConfigureMockMvc
 public class PostControlTest {
+
+    @MockBean
+    private PostRepository postRepository;
 
     @Autowired
     private MockMvc mockMvc;
@@ -28,9 +38,12 @@ public class PostControlTest {
     @Test
     @WithMockUser
     public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/create"))
+        this.mockMvc.perform(post("/save")
+                .param("name", "Куплю ладу-грант. Дорого."))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(view().name("/create"));
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(postRepository).save(argument.capture());
+        assertThat(argument.getValue().getName(), is("Куплю ладу-грант. Дорого."));
     }
 }
