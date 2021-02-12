ad12b900f6cd32dbaeab8e3e3396b93382216213
fruit <a.s.kirson@gmail.com>
2021-02-13 09:31:52 +1000

    0. Spring Liquibase [#364863]

--- src/main/resources/db/posts.sql
+++ src/main/resources/db/posts.sql
@@ -4,16 +4,3 @@ CREATE TABLE IF NOT EXISTS posts (
   description text,
   created timestamp without time zone default now()
 );
-
-CREATE TABLE users (
-  username VARCHAR(50) NOT NULL,
-  password VARCHAR(100) NOT NULL,
-  enabled boolean default true,
-  PRIMARY KEY (username)
-);
-
-CREATE TABLE authorities (
-  id serial primary key,
-  authority VARCHAR(50) NOT NULL,
-  FOREIGN KEY (username) REFERENCES users(username)
-);
---
+++ src/main/resources/db/sheme.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE IF NOT EXISTS users (
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  enabled boolean default true,
+  PRIMARY KEY (username)
+);
+
+CREATE TABLE IF NOT EXISTS authorities (
+  id serial primary key,
+  authority VARCHAR(50) NOT NULL,
+  FOREIGN KEY (username) REFERENCES users(username)
+);
