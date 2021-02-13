23c2a2b18b824db6b69b140e5fe47cd01ae79b2f
fruit <a.s.kirson@gmail.com>
2021-02-13 11:29:00 +1000

    0. Spring Liquibase [#364863]

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -1,6 +1,4 @@
 insert into posts (name) values ('О чем этот форум?');
 insert into posts (name) values ('Правила форума.');
 
-insert into authorities(authority) values ('ROLE_USER');
-insert into authorities(authority) values ('ROLE_ADMIN');
 
